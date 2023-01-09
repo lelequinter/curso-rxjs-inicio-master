f29c4c7be1b61d73153d7e31fce5ea340a977fdf
Leyder Quintero <lele.quinter@gmail.com>
2023-01-09 18:42:06 -0500

    tap

---
+++ src/operadores/03-tap.ts
@@ -0,0 +1,12 @@
+import { tap, range, map } from 'rxjs';
+
+const numeros$ = range(1, 5);
+
+numeros$.pipe(
+    tap(x => console.log('antes', x)),
+    map(val => val * 10),
+    tap({
+        next: valor => console.log('despues', valor),
+        complete: () => console.log('Se termino todo')
+    })
+).subscribe(val => console.log('subs', val));
