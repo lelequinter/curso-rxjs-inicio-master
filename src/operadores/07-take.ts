79624b52a4839e43c4f672e07b9c9702b3bb6e21
Leyder Quintero <lele.quinter@gmail.com>
2023-01-11 20:44:26 -0500

    take

---
+++ src/operadores/07-take.ts
@@ -0,0 +1,17 @@
+import { of, take, tap } from "rxjs";
+
+const numeros$ = of(1, 2, 3, 4, 5).pipe(
+    tap(t => console.log('tap de of::', t))
+);
+
+numeros$
+    .pipe(
+        tap(t => console.log('tap::', t)),
+        take(3)
+    )
+    .subscribe({
+        next: val => console.log('next::', val),
+        complete: () => console.log('compplete')
+
+
+    })
