646c277b61378fbf59cffc9bf39b2862c001221f
lelequinter <lele.quinter@gmail.com>
2023-01-27 14:19:37 -0500

    concat-funcion

---
+++ src/combinacion/03-concat-function.ts
@@ -0,0 +1,8 @@
+import { concat, interval, of, take } from "rxjs";
+const interval$ = interval(1000);
+
+concat(
+    interval$.pipe(take(3)), 
+    interval$.pipe(take(2)),
+    of(true)
+).subscribe(console.log);
