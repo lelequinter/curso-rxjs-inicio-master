7a9a6874ac608d4e6c8cdd50404a2eef244625b7
lelequinter <lele.quinter@gmail.com>
2023-01-27 11:06:09 -0500

    endWith

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { of, startWith } from "rxjs";
+import { endWith, of, startWith } from "rxjs";
 
 const numeros$ = of(1,2,3).pipe(
-    startWith('a')
+    startWith('a','b','c'),
+    endWith('x','y','z'),
 );
 
 numeros$.subscribe(console.log)
